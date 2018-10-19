b5f55b12c0b321715e32e1e5189cfaca956a6ab9
BHARATH KUMAR GOUD SATHURI <sathuribharathbrothers1234@gmail.com>
2018-10-19 10:13:03 +0530

    ASU DATABASE CONCEPTS 1ST ASSIGNMENT

--- solution.sql
+++ solution.sql
@@ -1,55 +1,35 @@
-CREATE TABLE query1 AS
-
-SELECT GENRES.NAME AS name,COUNT(HASAGENRE.movieid) AS moviecount FROM
-
-HASAGENRE INNER JOIN GENRES ON
-
-GENRES.GENREID=HASAGENRE.genreid
-
-GROUP BY GENRES.NAME;
-
-CREATE TABLE query2 AS 
-select GENRES.name as name , avg(ratings.rating) as rating 
-from  hasagenre INNER JOIN ratings  ON hasagenre.movieid = ratings.movieid
-INNER JOIN genres ON genres.genreid = hasagenre.genreid   group by GENRES.NAME
-;
-CREATE TABLE query3  AS
-select  movies.title as title, count(ratings.movieid) as countofratings 
-from movies INNER JOIN ratings
-ON movies.movieid = ratings.movieid 
-group by movies.title
-having count(ratings.movieid)>=10 
-;
-CREATE TABLE query5 AS 
-select title as title , avg(rating) as average 
-from movies , ratings 
-where movies.movieid = ratings.movieid group by movies.movieid
-;
-CREATE TABLE query6 AS 
-
-select avg(r.rating) as average from ratings r, genres g, hasagenre h
-
- where r.movieid = h.movieid and g.genreid = h.genreid and g.name = 'Comedy';
- 
-CREATE TABLE query7 AS 
-
-select avg(r.rating) as average  from ratings r, genres g, hasagenre h
-
- where r.movieid = h.movieid and g.genreid = h.genreid and g.name = 'Comedy' 
- 
- and  
-(select avg(r.rating) as average  from ratings r, genres g, hasagenre h
-
- where r.movieid = h.movieid and g.genreid = h.genreid and g.name = 'Romance' );
-
-CREATE TABLE query8 AS 
-
-select avg(r.rating) as average  from ratings r, genres g, hasagenre h
-
- where r.movieid = h.movieid and g.genreid = h.genreid and g.name = 'Comedy'
-
- and not   
- 
- (select avg(r.rating) as average  from ratings r, genres g, hasagenre h
-
- where r.movieid = h.movieid and g.genreid = h.genreid and g.name ='Romance');
+CREATE TABLE users(
+   USERID INT PRIMARY KEY NOT NULL,
+   NAME TEXT NOT NULL
+);
+CREATE TABLE movies(
+   MOVIEID INT PRIMARY KEY NOT NULL,
+   TITLE TEXT NOT NULL
+);
+CREATE TABLE taginfo(
+   TAGID INT PRIMARY KEY NOT NULL,
+   CONTENT TEXT NOT NULL
+);
+CREATE TABLE genres(
+   GENREID INT PRIMARY KEY NOT NULL,
+   NAME TEXT NOT NULL
+);
+CREATE TABLE ratings(
+   USERID INT REFERENCES USERS(USERID),
+   MOVIEID INT REFERENCES MOVIES(MOVIEID),
+   RATING NUMERIC NOT NULL CHECK(RATING>=0 AND RATING<=5),
+   TIMESTAMP BIGINT NOT NULL,
+   PRIMARY KEY (USERID, MOVIEID)
+);
+CREATE TABLE tags(
+   USERID INT REFERENCES USERS(USERID),
+   MOVIEID INT REFERENCES MOVIES(MOVIEID),
+   TAGID INT REFERENCES TAGINFO(TAGID),
+   TIMESTAMP BIGINT NOT NULL,
+   PRIMARY KEY (USERID, MOVIEID, TAGID)
+);
+CREATE TABLE HASAGENRE(
+   MOVIEID INT REFERENCES MOVIES(MOVIEID),
+   GENREID INT REFERENCES GENRES(GENREID),
+   PRIMARY KEY (MOVIEID, GENREID)   
+);
